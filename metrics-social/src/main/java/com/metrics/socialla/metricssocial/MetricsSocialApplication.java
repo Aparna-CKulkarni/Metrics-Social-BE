24094123aa359fc4722459dd4bd1752a33e80308
Aparna <aparna@metricsatsocial.in>
2023-04-17 23:40:48 +0530

    initial commit

---
+++ metrics-social/src/main/java/com/metrics/socialla/metricssocial/MetricsSocialApplication.java
@@ -0,0 +1,13 @@
+package com.metrics.socialla.metricssocial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetricsSocialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetricsSocialApplication.class, args);
+	}
+
+}
---
+++ metrics-social/src/test/java/com/metrics/socialla/metricssocial/MetricsSocialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.metrics.socialla.metricssocial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetricsSocialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
